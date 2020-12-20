69a5e3cdd1e5edbf6e917bf62ffe415d988014f9
Clément Décou <clementdecou@macbook-pro-de-clement.home>
2020-12-20 14:05:53 +0100

    feat: add acf utils

--- src/Admin/ThemeSettings.php
+++ src/Admin/ThemeSettings.php
@@ -2,11 +2,14 @@
 
 namespace ClementCore\Admin;
 
+use ClementCore\Utils\AcfUtils;
+use Simply\Core\Contract\CacheInterface;
+
 class ThemeSettings {
     static $isLive;
 
     static function getRs() {
-        return get_field('cd_theme-settings_rs', 'option');
+        return AcfUtils::getRepeaterOption('cd_theme-settings_rs');
     }
 
     /**
@@ -20,15 +23,22 @@ class ThemeSettings {
         return self::$isLive;
     }
 
-    static function getPresentationData() {
-         return [
-             'mainText' => get_field('cd-_theme-settings_home-presentation', 'option'),
-             'subtext' => get_field('cd-_theme-settings_home-sub-presentation', 'option'),
-             'callToAction' => get_field('cd-_theme-settings_home-presentation-call-to-action', 'option')
-        ];
+    static function getPresentationData(CacheInterface $cache) {
+        $contentCache = $cache->get('home-presentation');
+        if (!$contentCache) {
+            $return =  [
+                'mainText' => get_field('cd-_theme-settings_home-presentation', 'option'),
+                'subtext' => get_field('cd-_theme-settings_home-sub-presentation', 'option'),
+                'callToAction' => get_field('cd-_theme-settings_home-presentation-call-to-action', 'option')
+            ];
+            $cache->set('home-presentation', $return);
+        } else {
+            $return = $contentCache;
+        }
+        return $return;
     }
 
     static function getHomeServices() {
-        return get_field('cd-_theme-settings_home-services', 'option');
+        return AcfUtils::getRepeaterOption('cd-_theme-settings_home-services');
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,7 @@ use ClementCore\Admin\ThemeSettings;
 use ClementCore\Api\TokenGenerator;
 use ClementCore\Repository\ProjectRepository;
 use ClementCore\Repository\ProjectTagRepository;
+use Simply\Core\Contract\CacheInterface;
 use Simply\Core\Repository\PostRepository;
 use Simply\Core\Template\TemplateEngine;
 use Theme2020\Models\Menu\Menu;
@@ -14,11 +15,13 @@ use Theme2020\Models\Menu\Menu;
 class HomeController extends BaseController {
     private $projectTagRepository;
     private $projectRepository;
+    private $cache;
 
-    public function __construct(TemplateEngine $engine, ProjectTagRepository $projectTagRepository, ProjectRepository $projectRepository) {
+    public function __construct(TemplateEngine $engine, ProjectTagRepository $projectTagRepository, ProjectRepository $projectRepository, CacheInterface $cache) {
         parent::__construct($engine);
         $this->projectTagRepository = $projectTagRepository;
         $this->projectRepository = $projectRepository;
+        $this->cache = $cache;
     }
 
     public function home() {
@@ -28,7 +31,7 @@ class HomeController extends BaseController {
         $this->render('page/home.html.twig', [
             'headerMenuItems' => $headerMenu->getItems(),
             'socialNetworks' => ThemeSettings::getRs(),
-            'presentation' => ThemeSettings::getPresentationData(),
+            'presentation' => ThemeSettings::getPresentationData($this->cache),
             'services' => ThemeSettings::getHomeServices(),
             'project_tags' => $this->projectTagRepository->findBy(['hide_empty' => true]),
             'projects' => $this->projectRepository->findBy([], ['meta_key' => 'cd-project_grid-position', 'order' => 'ASC'])
