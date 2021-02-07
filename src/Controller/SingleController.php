0fa6ca347034810c804d25fd58be0dc118bdd24b
Clément Décou <clementdecou@macbook-pro-de-clement.home>
2021-02-07 17:35:25 +0100

    feat: start integrate post related

--- src/Controller/SingleController.php
+++ src/Controller/SingleController.php
@@ -2,12 +2,26 @@
 
 namespace ClementCore\Controller;
 
+use ClementCore\Model\Post;
+use ClementCore\Repository\PostRepository;
 use Simply\Core\Query\SimplyQuery;
+use Simply\Core\Template\TemplateEngine;
 
 class SingleController extends BaseController {
+    protected $postRepository;
+
+    public function __construct(TemplateEngine $engine, PostRepository $postRepository) {
+        parent::__construct($engine);
+        $this->postRepository = $postRepository;
+    }
 
     public function post() {
+        /** @var Post $currentPost */
         $currentPost = SimplyQuery::getCurrentObject();
-        $this->render('single/post.html.twig', ['currentPost' => $currentPost, 'postTags' => $currentPost->getTags()]);
+        $this->render('single/post.html.twig', [
+            'currentPost' => $currentPost,
+            'postTags' => $currentPost->getTags(),
+            'relatedPosts' => $this->postRepository->getRelatedPosts($currentPost->getID(), ['limit' => 3]),
+        ]);
     }
 }
