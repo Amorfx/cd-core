ca14564a30dc77ae87bbcfc26ceed0368963d773
Clément Décou <clementdecou@macbook-pro-de-clement.home>
2021-02-21 15:29:28 +0100

    feat: finish site ?

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,21 +5,24 @@ namespace ClementCore\Controller;
 use ClementCore\Admin\ThemeSettings;
 use ClementCore\Model\Post;
 use ClementCore\Utils\Paginator;
+use Simply\Core\Model\TermObject;
 use Simply\Core\Query\SimplyQuery;
 
 class BlogController extends BaseController {
-    /**
-     * Function called in infinite load in blog page
-     */
-    public function getPosts() {
-
-    }
-
     public function index() {
-        /** @var Post[] $allPosts */
         $allPosts = SimplyQuery::getCurrentQuery()->getQueriedPosts();
         $search = get_query_var('s');
         $paginator = new Paginator();
-        $this->render('page/blog.html.twig', ['allPosts' => $allPosts, 'pagination' => $paginator->generateOptimizedPagination(), 'keywords' => $search]);
+        $currentObject = SimplyQuery::getCurrentObject();
+        $isAuthor = is_author();
+        $this->render('page/blog.html.twig', ['allPosts' => $allPosts, 'pagination' => $paginator->generateOptimizedPagination(), 'keywords' => $search, 'isAuthor' => $isAuthor, 'currentObject' => $currentObject]);
+    }
+
+    public function taxonomy() {
+        /** @var TermObject $term */
+        $term = SimplyQuery::getCurrentObject();
+        $allPosts = SimplyQuery::getCurrentQuery()->getQueriedPosts();
+        $paginator = new Paginator();
+        $this->render('page/blog.html.twig', ['allPosts' => $allPosts, 'pagination' => $paginator->generateOptimizedPagination(), 'term' => $term]);
     }
 }
